ba7d4596c5b123fa151fdb654307a4fee2a02a93
dom <domagoj@l8bloom.hr>
2026-03-11 17:54:38 +0100

    feat: add image generation example

---
+++ examples/image_gen/image_gen.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	sd "github.com/l8bloom/gosd"
+)
+
+func main() {
+	// load the dynamic libraries
+	sd.Load()
+
+	// create and conifgure the inference context
+	ctxParams := sd.ContextParamsInit()
+
+	ctxParams.DiffusionModelPath = "/tmp/stable.diffusion/flux-2-klein-9b-Q8_0.gguf"
+	ctxParams.VAEPath = "/tmp/stable.diffusion/diffusion_pytorch_model.safetensors"
+	ctxParams.LLMPath = "/tmp/stable.diffusion/Qwen3-8B-Q8_0.gguf"
+
+	ctxParams.DiffusionFlashAttn = true // potential hardware optimizations
+	// ctxParams.KeepClipOnCPU = true // in case of lower vram
+
+	fmt.Printf("\nContext values:\n%s", sd.CtxParamsToStr(ctxParams))
+
+	ctx := sd.NewContext(ctxParams)
+	defer sd.FreeCtx(ctx)
+
+	// fetch some default image values and further configure
+	imgParams := sd.ImageGenParamsInit()
+
+	// prompts
+	imgParams.Prompt = "An orange cat on palm beach playing with oranges."
+	imgParams.NegativePrompt = "mascots, watermark, signature"
+
+	// sampler config
+	imgParams.SampleParams.SampleSteps = 20
+
+	// vram saving configuration in case of lower vram
+	imgParams.VAETilingParams.Enabled = true
+	imgParams.VAETilingParams.RelSizeX = 4
+	imgParams.VAETilingParams.RelSizeY = 4
+
+	// image resolution
+	imgParams.Width = 1536
+	imgParams.Height = 768
+
+	fmt.Printf("\nImage params:\n%s", sd.ImageGenParamsToStr(imgParams))
+
+	genImage := sd.GenerateImage(ctx, imgParams)
+	genImage.SavePNG("output.png")
+}
